f1bb2a22cacdacf546955f5a84c3eb77aa02a5be
infobarbosa <infobarbosa@gmail.com>
2024-08-25 17:44:05 -0300

    inclusão dos scripts com a solução parcial do módulo incluindo etapas anteriores.

---
+++ 04-modulos/assets/scripts/modules/glue-catalog/main.tf
@@ -0,0 +1,47 @@
+resource "aws_glue_catalog_database" "dataeng-glue-database" {
+  name = var.database_name
+}
+
+resource "aws_glue_catalog_table" "dataeng-glue-table-clientes" {
+  database_name = aws_glue_catalog_database.dataeng-glue-database.name
+  name          = "tb_raw_clientes"
+  table_type    = "EXTERNAL_TABLE"
+  parameters = {
+    classification = "csv",
+    "compressionType" = "gzip",
+    "skip.header.line.count" = "1"
+  }
+  storage_descriptor {
+    location = "s3://dataeng-modulo-1-260762633884-ecgjux/raw/clientes/"
+    input_format = "org.apache.hadoop.mapred.TextInputFormat"
+    output_format = "org.apache.hadoop.hive.ql.io.HiveIgnoreKeyTextOutputFormat"
+    compressed = false
+    number_of_buckets = -1
+    ser_de_info {
+      serialization_library = "org.apache.hadoop.hive.serde2.lazy.LazySimpleSerDe"
+      parameters = {
+        "field.delim" = ";"
+      }
+    }
+    columns {
+        name = "id"
+        type = "int"
+    }
+    columns {
+        name = "nome"
+        type = "string"
+    }
+    columns {
+        name = "data_nasc"
+        type = "date"
+    }
+    columns {
+        name = "cpf"
+        type = "string"
+    }
+    columns {
+        name = "email"
+        type = "string"
+    }  
+  }
+}
--- 04-modulos/modulos/main.tf
+++
@@ -1,9 +0,0 @@
-provider "aws" {
-  region = "us-east-1"
-}
-
-module "glue-catalog" {
-  source  = "./modules/glue-catalog"
-
-  database_name = "dataeng-modulo-3-glue-database"
-}
