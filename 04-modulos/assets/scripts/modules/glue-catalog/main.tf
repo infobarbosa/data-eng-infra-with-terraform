a13d594c33cf69757bd4970ef7682f961794d9d0
infobarbosa <infobarbosa@gmail.com>
2024-08-25 18:01:12 -0300

    Atualizar tabela do catálogo do AWS Glue para incluir dados de pedidos

--- 04-modulos/assets/scripts/modules/glue-catalog/main.tf
+++ 04-modulos/assets/scripts/modules/glue-catalog/main.tf
@@ -45,3 +45,61 @@ resource "aws_glue_catalog_table" "dataeng-glue-table-clientes" {
     }  
   }
 }
+
+resource "aws_glue_catalog_table" "dataeng-glue-table-pedidos" {
+    database_name = aws_glue_catalog_database.dataeng-glue-database.name
+    name          = "tb_raw_pedidos"
+    table_type    = "EXTERNAL_TABLE"
+    parameters = {
+        classification = "csv",
+        "compressionType" = "gzip",
+        "skip.header.line.count" = "1"
+    }
+    storage_descriptor {
+        location = "s3://dataeng-modulo-1-260762633884-ecgjux/raw/pedidos/"
+        input_format = "org.apache.hadoop.mapred.TextInputFormat"
+        output_format = "org.apache.hadoop.hive.ql.io.HiveIgnoreKeyTextOutputFormat"
+        compressed = false
+        number_of_buckets = -1
+        ser_de_info {
+        serialization_library = "org.apache.hadoop.hive.serde2.lazy.LazySimpleSerDe"
+        parameters = {
+            "field.delim" = ";"
+        }
+        }
+        columns {
+            name = "id_pedido"
+            type = "string"
+        }
+
+        columns {
+            name = "produto"
+            type = "string"
+        }
+
+        columns {
+            name = "valor_unitario"
+            type = "float"
+        }
+
+        columns {
+            name = "quantidade"
+            type = "bigint"
+        }
+
+        columns {
+            name = "data_criacao"
+            type = "timestamp"
+        }      
+
+        columns {
+            name = "uf"
+            type = "string"
+        }
+
+        columns {
+            name = "id_cliente"
+            type = "bigint"
+        }  
+    } 
+}
