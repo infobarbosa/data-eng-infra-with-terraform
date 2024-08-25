02570af077a60e273b1f1442068dc89f2d527a6b
infobarbosa <infobarbosa@gmail.com>
2024-08-25 18:04:01 -0300

    Atualizar tabela do catálogo do AWS Glue para incluir dados de pedidos

--- 05-provisionamento-computacao/assets/scripts/modules/glue-catalog/main.tf
+++ 05-provisionamento-computacao/assets/scripts/modules/glue-catalog/main.tf
@@ -1,9 +1,9 @@
-resource "aws_glue_catalog_database" "dataeng_modulo_4_db" {
+resource "aws_glue_catalog_database" "dataeng-glue-database" {
   name = var.database_name
 }
 
-resource "aws_glue_catalog_table" "dataeng_modulo_4_tb_clientes" {
-  database_name = aws_glue_catalog_database.dataeng_modulo_4_db.name
+resource "aws_glue_catalog_table" "dataeng-glue-table-clientes" {
+  database_name = aws_glue_catalog_database.dataeng-glue-database.name
   name          = "tb_raw_clientes"
   table_type    = "EXTERNAL_TABLE"
   parameters = {
@@ -12,7 +12,7 @@ resource "aws_glue_catalog_table" "dataeng_modulo_4_tb_clientes" {
     "skip.header.line.count" = "1"
   }
   storage_descriptor {
-    location = "s3://dataeng-modulo-1-260762633884-gntxzg/raw/clientes/"
+    location = "s3://dataeng-modulo-1-260762633884-ecgjux/raw/clientes/"
     input_format = "org.apache.hadoop.mapred.TextInputFormat"
     output_format = "org.apache.hadoop.hive.ql.io.HiveIgnoreKeyTextOutputFormat"
     compressed = false
@@ -46,60 +46,60 @@ resource "aws_glue_catalog_table" "dataeng_modulo_4_tb_clientes" {
   }
 }
 
-resource "aws_glue_catalog_table" "dataeng_modulo_4_tb_pedidos" {
-  database_name = aws_glue_catalog_database.dataeng_modulo_4_db.name
-  name          = "tb_raw_pedidos"
-  table_type    = "EXTERNAL_TABLE"
-  parameters = {
-    classification = "csv",
-    "compressionType" = "gzip",
-    "skip.header.line.count" = "1"
-  }
-  storage_descriptor {
-    location = "s3://dataeng-modulo-1-260762633884-gntxzg/raw/pedidos/"
-    input_format = "org.apache.hadoop.mapred.TextInputFormat"
-    output_format = "org.apache.hadoop.hive.ql.io.HiveIgnoreKeyTextOutputFormat"
-    compressed = false
-    number_of_buckets = -1
-    ser_de_info {
-      serialization_library = "org.apache.hadoop.hive.serde2.lazy.LazySimpleSerDe"
-      parameters = {
-        "field.delim" = ";"
-      }
+resource "aws_glue_catalog_table" "dataeng-glue-table-pedidos" {
+    database_name = aws_glue_catalog_database.dataeng-glue-database.name
+    name          = "tb_raw_pedidos"
+    table_type    = "EXTERNAL_TABLE"
+    parameters = {
+        classification = "csv",
+        "compressionType" = "gzip",
+        "skip.header.line.count" = "1"
     }
-    columns {
-        name = "id_pedido"
-        type = "string"
-    }
+    storage_descriptor {
+        location = "s3://dataeng-modulo-1-260762633884-ecgjux/raw/pedidos/"
+        input_format = "org.apache.hadoop.mapred.TextInputFormat"
+        output_format = "org.apache.hadoop.hive.ql.io.HiveIgnoreKeyTextOutputFormat"
+        compressed = false
+        number_of_buckets = -1
+        ser_de_info {
+        serialization_library = "org.apache.hadoop.hive.serde2.lazy.LazySimpleSerDe"
+        parameters = {
+            "field.delim" = ";"
+        }
+        }
+        columns {
+            name = "id_pedido"
+            type = "string"
+        }
 
-    columns {
-        name = "produto"
-        type = "string"
-    }
+        columns {
+            name = "produto"
+            type = "string"
+        }
 
-    columns {
-        name = "valor_unitario"
-        type = "float"
-    }
+        columns {
+            name = "valor_unitario"
+            type = "float"
+        }
 
-    columns {
-        name = "quantidade"
-        type = "bigint"
-    }
+        columns {
+            name = "quantidade"
+            type = "bigint"
+        }
 
-    columns {
-        name = "data_criacao"
-        type = "timestamp"
-    }      
+        columns {
+            name = "data_criacao"
+            type = "timestamp"
+        }      
 
-    columns {
-        name = "uf"
-        type = "string"
-    }
+        columns {
+            name = "uf"
+            type = "string"
+        }
 
-    columns {
-        name = "id_cliente"
-        type = "bigint"
-    }  
-  } 
+        columns {
+            name = "id_cliente"
+            type = "bigint"
+        }  
+    } 
 }
