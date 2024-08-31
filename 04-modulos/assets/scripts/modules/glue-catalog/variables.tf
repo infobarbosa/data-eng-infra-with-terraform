a431481b45454e90f4d0c27e71aaf9b6fc7caddc
infobarbosa <infobarbosa@yahoo.com.br>
2024-08-31 15:43:52 -0300

    incluindo scripts terraform para referência.

--- 04-modulos/assets/scripts/modules/glue-catalog/variables.tf
+++ 04-modulos/assets/scripts/modules/glue-catalog/variables.tf
@@ -1,4 +1,9 @@
 variable "database_name" {
-  description = "Nome do banco de dados Glue"
+  description = "The name of the Glue database"
+  type        = string
+}
+
+variable "bucket_name" {
+  description = "The name of the S3 bucket"
   type        = string
 }
